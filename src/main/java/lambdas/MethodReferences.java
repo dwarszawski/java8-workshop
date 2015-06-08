2d8455c681ac22d2fbc26b39d19edfa5ba759cd2
dwarszawski <damian.warszawski@gmail.com>
2015-06-08 21:36:01 +0200

    minor fixes

--- src/main/java/lambdas/MethodReferences.java
+++ src/main/java/lambdas/MethodReferences.java
@@ -4,12 +4,15 @@ public class MethodReferences {
     private static Integer op1(Integer value) {
         return ++value;
     }
+
     private Integer op2(Integer value) {
         return ++value;
     }
 
-    SequenceLike<Integer> nextInt1 =  i -> MethodReferences.op1(i);
-    SequenceLike<Integer> nextInt2 = MethodReferences::op1;
+    MethodReferences instance = new MethodReferences();
+
+    SequenceLike<Integer> nextInt1 = MethodReferences::op1;
+    SequenceLike<Integer> nextInt2 = instance::op2;
     SequenceLike<Integer> nextInt3 = this::op2;
     SequenceLike<Integer> nextInt4 = i -> {
         return op1(i);
